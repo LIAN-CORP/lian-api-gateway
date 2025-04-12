6cd571941b510550f2c71bbd926b6ff40100de4c
3lPr1mo <erickenriquechaparro@gmail.com>
2025-04-12 17:25:51 -0500

    initial commit

---
+++ src/test/java/com/lian/marketing/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
