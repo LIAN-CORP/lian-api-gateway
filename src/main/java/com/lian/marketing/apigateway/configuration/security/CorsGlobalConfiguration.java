fe5fc3910c5819b1ddad1c524aabe57c3ccc636d
3lPr1mo <erickenriquechaparro@gmail.com>
2025-09-21 13:17:27 -0500

    refactor(config): move CORS configuration from properties file to WebFlux-based setup

---
+++ src/main/java/com/lian/marketing/apigateway/configuration/security/CorsGlobalConfiguration.java
@@ -0,0 +1,26 @@
+package com.lian.marketing.apigateway.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration cors = new CorsConfiguration();
+    cors.setAllowedOrigins(List.of("http://localhost:3000"));
+    cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    cors.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin"));
+    cors.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", cors);
+    return new CorsWebFilter(source);
+  }
+
+}
