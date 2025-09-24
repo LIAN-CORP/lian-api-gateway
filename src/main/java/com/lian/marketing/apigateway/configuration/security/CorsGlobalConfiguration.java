ac442760ffa7bf50a07f62977b65175da2960f93
3lPr1mo <erickenriquechaparro@gmail.com>
2025-09-23 22:05:22 -0500

    fix(config): add missing `Content-Disposition` header to CORS allowed headers

--- src/main/java/com/lian/marketing/apigateway/configuration/security/CorsGlobalConfiguration.java
+++ src/main/java/com/lian/marketing/apigateway/configuration/security/CorsGlobalConfiguration.java
@@ -16,7 +16,7 @@ public class CorsGlobalConfiguration {
     CorsConfiguration cors = new CorsConfiguration();
     cors.setAllowedOrigins(List.of("http://localhost:3000"));
     cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    cors.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+    cors.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Content-Disposition"));
     cors.setAllowCredentials(true);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", cors);
